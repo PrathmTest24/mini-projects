b0047c1bc6d6a9ccb070b9a35a20246095a354a6
unknown <upalkarp24@gmail.com>
2024-02-07 23:47:52 +0530

    Light-Dark-Mode #1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Random from './components/random-color/random';
+import LigthDark from './components/light-dark-mode/LigthDark';
 function App() {
   return (
     <>
-      <Random/>
+     
+      {/* Random color Genarate      */}
+      {/* <Random/> */}
+
+      {/* Light - dark - mode  */}
+       <LigthDark/>
     </>
     
   );
